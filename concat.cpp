f689c1870c3554b246f65a20b749b84ae082299f
luislortega <a18016323@alumnos.uady.mx>
2020-09-26 14:28:49 -0500

    some advancesss

--- concat.cpp
+++ concat.cpp
@@ -1,34 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-void concat(char[], char[]);
-int stringLenght(char *cadena);
-
 int main()
 {
-    char s1[50] = "lol";
-    char s2[30] = "weyy";
-    concat(s1, s2);
-    printf("\nConcated string is :%s", s1);
+    char const *str1 = "wee";
+    char const *str2 = "lol";
+    int i;
+
+    printf("\nEnter two strings :");
+
+    i = 0;
+    while (str1[i] == str2[i] && str1[i] != '\0')
+        i++;
+    if (str1[i] > str2[i])
+        printf("str1 > str2");
+    else if (str1[i] < str2[i])
+        printf("str1 < str2");
+    else
+        printf("str1 = str2");
     return (0);
-}
-
-void concat(char s1[], char s2[])
-{
-    int i, j;
-    i = strlen(s1);
-    for (j = 0; s2[j] != '\0'; i++, j++)
-    {
-        s1[i] = s2[j];
-    }
-    s1[i] = '\0';
-}
-
-int stringLenght(char *cadena)
-{
-    int contador = 0;
-    for (int i = 0; cadena[i] != '\0'; ++i){
-        contador = contador + 1;
-    }
-    return contador;
 }
--- gaussracionales.cpp
+++ gaussracionales.cpp
@@ -26,17 +26,19 @@ Es recomendable aplicar un procedimiento de simplificación, durante el proceso 
 
 struct racional
 {
-private:
-    char const *representacion;
-
-public:
-    char const *getRepresentacion();
-    void setRepresentacion(int,int);
-    void enteroaCadena(int,char*);
-    void cadenaaEntero(int*); //no construido
-    //Utilidades...
-    void concatenarCadenas(char[],char[]);
-    int tamanoCadena(char*);
+    private:
+        char const *representacion;
+    public:
+        char const *getRepresentacion();
+        void setRepresentacion(int,int);
+        void getRepresentacionSeparada();
+        //Utilidades...
+        void enteroaCadena(int,char*);
+        void cadenaaEntero(int*); //no construido
+        void concatenarCadenas(char[],char[]);
+        int tamanoCadena(char*);
+        int tamanoCadenaConstante(char const*);
+        void compararCadenas(char, char const *);
 };
 
 char const *racional::getRepresentacion()
@@ -44,12 +46,42 @@ char const *racional::getRepresentacion()
     return representacion;
 };
 
+void racional::compararCadenas(char  cadena1, char const *cadena2){
+    //printf(cadena1);
+    if(cadena1 == cadena2[0]){
+        printf("iguales \n");
+    }else{
+        printf("no iguales \n");
+    }
+};
+
+void racional::getRepresentacionSeparada(){
+    
+    int tamanoRepresentacion = tamanoCadenaConstante(representacion);
+    
+    printf("tamaño: %d \n",tamanoRepresentacion);
+
+    //for(int i = 0; i < tamanoRepresentacion; i++){
+
+        /*if(representacion[2] == "/"){
+            printf("loencontre");0
+        } */
+        char lol = representacion[tamanoRepresentacion-1];
+        char const *lol2 = "2";
+        compararCadenas(lol, lol2);
+
+        printf("posicion2: %c \n", lol);
+    //}
+};
+
+
 void racional::setRepresentacion(int numerador, int denominador)
 {
     char numeradorCadena[10];
     char separador[10] = "/";
     char denominadorCadena[10];
 
+    //Conversion de enteros a cadenasl
     enteroaCadena(numerador, numeradorCadena);
     enteroaCadena(denominador, denominadorCadena);
 
@@ -58,9 +90,19 @@ void racional::setRepresentacion(int numerador, int denominador)
     concatenarCadenas(numeradorCadena, denominadorCadena);
 
     representacion = numeradorCadena;
+    printf("representacion:%s \n",representacion);
+
 };
 
 int racional::tamanoCadena(char *cadena){
+    int contador = 0;
+    for (int i = 0; cadena[i] != '\0'; ++i){
+        contador = contador + 1;
+    }
+    return contador;
+};
+
+int racional::tamanoCadenaConstante(char const *cadena){
     int contador = 0;
     for (int i = 0; cadena[i] != '\0'; ++i){
         contador = contador + 1;
@@ -106,56 +148,13 @@ void racional::enteroaCadena(int entero, char *posicionCadena)
     }
 };
 
-void inttochar(char *buf2);
 
 int main()
 {
     racional r1;
     char buffer[50];
-
     char const *buffer2;
-
-    char algo[50];
-
-    inttochar(algo);
-
-    r1.setRepresentacion(1, 2);
+    r1.setRepresentacion(11, 22);
     buffer2 = r1.getRepresentacion();
-
-    buffer2 = algo;
-}
-
-void inttochar(char *buf2)
-{
-    int n = 124;
-    char buffer[50];
-    int i = 0;
-
-    bool isNeg = n < 0;
-
-    unsigned int n1 = isNeg ? -n : n;
-
-    while (n1 != 0)
-    {
-        buf2[i++] = n1 % 10 + '0';
-        n1 = n1 / 10;
-    }
-
-    if (isNeg)
-        buf2[i++] = '-';
-
-    buf2[i] = '\0';
-
-    for (int t = 0; t < i / 2; t++)
-    {
-        buf2[t] ^= buf2[i - t - 1];
-        buf2[i - t - 1] ^= buf2[t];
-        buf2[t] ^= buf2[i - t - 1];
-    }
-
-    if (n == 0)
-    {
-        buf2[0] = '0';
-        buf2[1] = '\0';
-    }
+    r1.getRepresentacionSeparada();
 }
